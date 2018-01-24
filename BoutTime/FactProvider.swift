dfc9d59e22a3860da382094c3ad9508f219b1150
Nick Fialkowski <nikko444@gmail.com>
2018-01-23 23:39:49 -0500

    Finished picking up the facts for the quiz

--- BoutTime/FactProvider.swift
+++ BoutTime/FactProvider.swift
@@ -43,7 +43,94 @@ class FactProvider {
                 date: DateComponents(year: 1920, month: 4, day: 7)),
         
         FactModel(caption: "First air navigation beacon", //MARK: 11. First navigation beacon
-                date: DateComponents(year: 1926, month: 12, day: 26))
+                date: DateComponents(year: 1926, month: 12, day: 7)),
+        
+        FactModel(caption: "First transatlantic non-stop solo flight", //MARK: 12. First transatlantic non-stop solo flight
+                date: DateComponents(year: 1927, month: 5, day: 20)),
+        
+        FactModel(caption: "World famous airship \"Graf Zeppelin\" first time flew", //MARK: 13. Zeppelin
+                date: DateComponents(year: 1928, month: 9, day: 18)),
+        
+        FactModel(caption: "First radio controlled airport tower", //MARK: 14. First radio controlled airport tower
+                date: DateComponents(year: 1930, month: 12, day: 31)),
+        
+        FactModel(caption: "First solo flight around the world", //MARK: 15. First solo flight around the world
+                date: DateComponents(year: 1933, month: 7, day: 15)),
+        
+        FactModel(caption: "McDonnell Douglas DC-3 - one of the most successful aircrafts in history first flew", //MARK: 16. Douglas DC-3
+                date: DateComponents(year: 1935, month: 12, day: 17)),
+        
+        FactModel(caption: "First flight by a fully pressurized airplane, the Lockheed XC-35", //MARK: 17. A fully pressurized airplane
+                date: DateComponents(year: 1937, month: 5, day: 7)),
+        
+        FactModel(caption: "New York Municipal Airport opens. \'La Guardia\' nowdays", //MARK: 18. La Guardia
+                date: DateComponents(year: 1939, month: 12, day: 2)),
+        
+        FactModel(caption: "Battle of Britain - first major military campaign fought entirely by air forces", //MARK: 19. Battle of Britain
+                date: DateComponents(year: 1940, month: 7, day: 10)),
+        
+        FactModel(caption: "First man-made object to travel into space by crossing the Kármán line", //MARK: 20. First man-made object to travel into space
+                date: DateComponents(year: 1944, month: 6, day: 20)),
+        
+        FactModel(caption: "World's first commercial jetliner", //MARK: 21. First commercial jetliner
+                date: DateComponents(year: 1949, month: 7, day: 27)),
+        
+        FactModel(caption: "First intercontinental ballistic missile (ICBM)", //MARK: 22. First ICMB
+                date: DateComponents(year: 1957, month: 8, day: 21)),
+        
+        FactModel(caption: "First animal in orbit, the dog Laika", //MARK: 23. First animal in orbit
+                date: DateComponents(year: 1957, month: 11, day: 3)),
+        
+        FactModel(caption: "World's first long-distance commercial jetliner", //MARK: 24. First commercial long-range jetliner
+                date: DateComponents(year: 1957, month: 12, day: 20)),
+        
+        FactModel(caption: "First reaching Earth escape velocity or Trans Lunar Injection", //MARK: 25. First reaching Earth escape velocity
+                date: DateComponents(year: 1959, month: 1, day: 2)),
+        
+        FactModel(caption: "First artificial satellite to reach the Moon vicinity", //MARK: 26. First artificial satellite to reach the Moon vicinity
+                date: DateComponents(year: 1959, month: 1, day: 4)),
+        
+        FactModel(caption: "First photograph of Earth from orbit", //MARK: 27. First photograph of Earth from orbit
+                date: DateComponents(year: 1959, month: 8, day: 7)),
+        
+        FactModel(caption: "First impact into another world (the Moon)", //MARK: 28. First impact into another world (the Moon)
+                date: DateComponents(year: 1959, month: 9, day: 13)),
+        
+        FactModel(caption: "First photos of another world from space: The far side of the Moon", //MARK: 29. First photos of another world from space
+                date: DateComponents(year: 1959, month: 10, day: 4)),
+        
+        FactModel(caption: "First solar probe", //MARK: 30. First solar probe
+                date: DateComponents(year: 1960, month: 3, day: 1)),
+        
+        FactModel(caption: "First plants and animals to return alive from Earth orbit", //MARK: 31. First plants and animals to return alive from Earth orbit
+                date: DateComponents(year: 1960, month: 8, day: 19)),
+        
+        FactModel(caption: "First orbital solar observatory", //MARK: 32. First orbital solar observatory
+                date: DateComponents(year: 1962, month: 3, day: 7)),
+        
+        FactModel(caption: "First spacecraft to impact the far side of the Moon", //MARK: 33. First spacecraft to impact the far side of the Moon
+                date: DateComponents(year: 1962, month: 4, day: 26)),
+        
+        FactModel(caption: "First woman in space", //MARK: 34. First woman in space
+                date: DateComponents(year: 1963, month: 7, day: 16)),
+        
+        FactModel(caption: "First extra-vehicular activity", //MARK: 35. First extra-vehicular activity
+                date: DateComponents(year: 1965, month: 3, day: 18)),
+        
+        FactModel(caption: "First close-up photographs of another planet: Mars", //MARK: 36. First close-up photographs of another planet: Mars
+                date: DateComponents(year: 1965, month: 7, day: 14)),
+        
+        FactModel(caption: "First soft landing on another world (the Moon)", //MARK: 37. First soft landing on another world
+                date: DateComponents(year: 1966, month: 2, day: 3)),
+        
+        FactModel(caption: "First impact into another planet (Venus)", //MARK: 38. First impact into another planet (Venus)
+                date: DateComponents(year: 1966, month: 3, day: 1)),
+        
+        FactModel(caption: "First piloted orbital mission of another celestial body (Moon)", //MARK: 39. First piloted orbital mission of the Moon
+                date: DateComponents(year: 1968, month: 12, day: 21)),
+        
+        FactModel(caption: "First human on the Moon, and first space launch from a celestial body other than the Earth", //MARK: 40. First human on the Moon, and first space launch from a celestial body other than the Earth
+                date: DateComponents(year: 1969, month: 7, day: 20))
 
     ]
     
